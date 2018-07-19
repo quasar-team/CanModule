101ecf2246659637b1691f7acfab362fed0f02cc
Michael Ludwig <michael.ludwig@cern.ch>
2018-07-19 12:03:13 +0200

    .

--- CanLibLoader/src/CanLibLoaderWin.cpp
+++ CanLibLoader/src/CanLibLoaderWin.cpp
@@ -68,7 +68,7 @@ namespace CanModule
 		} else {
 			LOG(Log::ERR) << "could not load the dynamic library: [" << ss.str() << "]";
 			// ErrorExit(TEXT("could not load the lib "));
-			string msg = "Error: could not load the dynamic library" + ss;
+			string msg = string("Error: could not load the dynamic library") + ss;
 			throw std::runtime_error( msg.c_str() );
 		}
 	}
