91d8f3ed48909215d8fb5330073267bc8d22fa6a
Michael Ludwig <michael.ludwig@cern.ch>
2018-07-19 16:32:46 +0200

    debugging for anagate parameters

--- CanInterface/include/CCanAccess.h
+++ CanInterface/include/CCanAccess.h
@@ -149,8 +149,10 @@ namespace CanModule
 			string temporalString;
 			while (getline(nameSS, temporalString, ':')) {
 				stringVector.push_back(temporalString);
+				LOG(Log::TRC) << "CCanAccess::parcerNameAndPar: stringVector new element= " << temporalString;
 			}
 			m_CanParameters.scanParameters(parameters);
+			LOG(Log::TRC) << "CCanAccess::parcerNameAndPar: stringVector size= " << stringVector.size();
 
 			return stringVector;
 		}
