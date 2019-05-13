5dbdd98d34e234632d72748b60a216f3e8c6818e
Michael Ludwig <michael.ludwig@cern.ch>
2019-05-13 14:12:08 +0200

    MLOGST

--- CanLibLoader/src/CanLibLoaderWin.cpp
+++ CanLibLoader/src/CanLibLoaderWin.cpp
@@ -121,6 +121,9 @@ namespace CanModule
 	{
 		LOG(Log::TRC, lh) << __FUNCTION__ << ": Accessing method get getCanBusAccess";
 		f_CCanAccess *canAccess = (f_CCanAccess *)GetProcAddress(m_pDynamicLibrary, "getCanBusAccess");
+
+		std::cout << __FILE__ << " " << __LINE__ << " " << __LINE__ << " CanAccess= 0x" << canAccess << std::endl;
+
 		// We check for errors again. If there is an error the library is released from memory.
 		if (!canAccess) {
 			throw std::runtime_error( string(__FUNCTION__) + string(": Error: could not locate the function"));
@@ -128,7 +131,6 @@ namespace CanModule
 		// We call the function getHalAccess we got from the library. This will give us a pointer to an object, wich we store.
 		LOG(Log::TRC, lh) << __FUNCTION__ << ": getCanBusAccess: got an object ptr from library, OK";
 
-		std::cout << __FILE__ << " " << __LINE__ << " " << __LINE__ << " CanModule::CCanAccess = 0x" << (CCanAccess*)(canAccess()) << std::endl;
 
 		return (CCanAccess*)(canAccess());
 	}
