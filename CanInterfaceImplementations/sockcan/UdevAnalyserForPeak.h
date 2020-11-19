bc2a6433eeb1c0d3148ad9498314ad1fddb2f5b1
Michael Ludwig <michael.ludwig@cern.ch>
2020-11-19 10:55:32 +0100

    missing files

---
+++ CanInterfaceImplementations/sockcan/UdevAnalyserForPeak.h
@@ -0,0 +1,71 @@
+/*
+ * UdevAnalyserForPeak.h
+ *
+ *  Created on: Mar 12, 2020
+ *      Author: mludwig
+ */
+
+#ifndef CANMODULE_CANINTERFACEIMPLEMENTATIONS_SOCKCAN_UDEVANALYSERFORPEAK_H_
+#define CANMODULE_CANINTERFACEIMPLEMENTATIONS_SOCKCAN_UDEVANALYSERFORPEAK_H_
+
+
+#include <string>
+#include <vector>
+
+#include <LogIt.h>
+
+namespace udevanalyserforpeak_ns {
+
+class UdevAnalyserForPeak {
+public:
+	typedef struct {
+		std::string link;
+		unsigned int localCanPort; // local: i.e. can0, can1
+		unsigned int systemDeviceIndex; // global: i.e. pcan-usb_pro_fd/0
+		unsigned int peakDriverNumber; // global: i.e. pcan33
+		int deviceID; // global, must be unique (serial#), i.e. devid=9054, -1 if not found for secondary ports
+		unsigned int socketNumber;
+	} PEAK_device_t;
+
+	/** factory providing one singleton
+	 * todo: use fancy threadsafe STATIC method
+	 */
+
+	static UdevAnalyserForPeak* getInstance() {
+		if ( !m_instance_ptr ) {
+			//std::cout << "creating singleton for UdevAnalyserForPeak class" << std::endl;
+			m_instance_ptr = new UdevAnalyserForPeak;
+		} else {
+			//std::cout << "reusing existing singleton for UdevAnalyserForPeak class" << std::endl;
+		}
+		return m_instance_ptr;
+	}
+
+	static std::string peakExtendedIdentifierToSocketCanDevice( std::string portId );
+	static void showMap();
+
+private:
+	// singleton, allocated at compile time, static.
+	static UdevAnalyserForPeak *m_instance_ptr;
+	UdevAnalyserForPeak(); // private constructor singleton
+	UdevAnalyserForPeak(const UdevAnalyserForPeak&); // no copy
+	virtual ~UdevAnalyserForPeak(){};
+
+	static std::vector<PEAK_device_t> m_peak_v;
+	static Log::LogComponentHandle m_logItHandleSock;
+
+	void m_createUdevPortMap(); // udev calls in constructor
+
+	unsigned int m_peakDeviceId( std::string s );
+	unsigned int m_peakSystemDeviceIndex( std::string s );
+	unsigned int m_peakLocalCanPort( std::string s );
+	unsigned int m_peakDriverNumber( std::string s );
+	unsigned int m_peakDeviceIdFromSystemDeviceIndex( unsigned int sd );
+
+	static bool m_peakStructCompare( PEAK_device_t peak1, PEAK_device_t peak2 ); // sort method
+
+};
+
+} /* namespace udevanalyserforpeak_ns */
+
+#endif /* CANMODULE_CANINTERFACEIMPLEMENTATIONS_SOCKCAN_UDEVANALYSERFORPEAK_H_ */
