f5374d2fd9653b963b719519afa002856fbd5786
Benjamin Farnham <bfarnham@dev-x64-cc7.cern.ch>
2020-12-09 14:43:12 +0100

    status & vendor info should be bitwise OR-ed (not AND-ed)

--- CanInterfaceImplementations/anagate/AnaCanScan.h
+++ CanInterfaceImplementations/anagate/AnaCanScan.h
@@ -90,7 +90,7 @@ public:
 	 */
 	uint32_t getPortStatus(){
 		AnaInt32 state = CANDeviceConnectState( m_UcanHandle );
-		return( state & CANMODULE_STATUS_BP_ANAGATE );
+		return( state | CANMODULE_STATUS_BP_ANAGATE );
 	}
 
 	virtual void setReconnectBehavior( CanModule::ReconnectAutoCondition cond, CanModule::ReconnectAction action ){
