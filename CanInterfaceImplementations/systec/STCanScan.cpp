a97d78947d0742f67df309a88a083236473e5030
Michael Ludwig <michael.ludwig@cern.ch>
2020-12-10 13:24:45 +0100

    all channels on module/hard reset systec @ windows not implemented

--- CanInterfaceImplementations/systec/STCanScan.cpp
+++ CanInterfaceImplementations/systec/STCanScan.cpp
@@ -391,7 +391,7 @@ bool STCanScan::sendErrorCode(long status)
  * hard reset the bridge and reconnect all ports and handlers: windows
  */
 /* static */ int STCanScan::reconnectAllPorts( tUcanHandle h ){
-	MLOGST(WRN,this) << __FUNCTION__ << " hard reset not yet implemented";
+	//MLOGST(WRN,this) << __FUNCTION__ << " hard reset not yet implemented";
 	std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << " hard reset not yet implemented";
 #if 0
 	/// have to call these two API methods
