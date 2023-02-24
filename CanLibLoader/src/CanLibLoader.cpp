00f2bf793fcb970ba91d171d3c4aafc156e75a51
Michael Ludwig <Michael.Ludwig@cern.ch>
2023-02-24 14:55:43 +0100

    debug

--- CanLibLoader/src/CanLibLoader.cpp
+++ CanLibLoader/src/CanLibLoader.cpp
@@ -41,8 +41,12 @@ CanLibLoader::CanLibLoader(const std::string& libName)
 	std::cout << __FUNCTION__ << " " << __FILE__ << " " << __LINE__ << " constructor " << libName << std::endl;
 	LogItInstance *logIt = LogItInstance::getInstance();
 	std::cout << __FUNCTION__ << " " << __FILE__ << " " << __LINE__ << " constructor " << libName << std::endl;
-	logIt->getComponentHandle( CanModule::LogItComponentName, lh );
-	std::cout << __FUNCTION__ << " " << __FILE__ << " " << __LINE__ << " constructor " << libName << std::endl;
+	if ( logIt != NULL ){
+		logIt->getComponentHandle( CanModule::LogItComponentName, lh );
+		std::cout << __FUNCTION__ << " " << __FILE__ << " " << __LINE__ << " constructor " << libName << std::endl;
+	} else {
+		std::cout << __FUNCTION__ << " " << __FILE__ << " " << __LINE__ << " logIt instance is NULL" << std::endl;
+	}
 }
 
 
