e98f75d6d13bad0df4857ff1313cc2135105ac5f
Michael Ludwig <Michael.Ludwig@cern.ch>
2023-03-14 10:34:08 +0100

    globalErrorSignal implementation: connect and fire

--- CanLibLoader/include/CanLibLoader.h
+++ CanLibLoader/include/CanLibLoader.h
@@ -51,23 +51,21 @@ class GlobalErrorSignaler /* singleton */
 private:
 	GlobalErrorSignaler(){};
 	~GlobalErrorSignaler();
-#if 0
-	{
-		globalErrorSignal.disconnect_all_slots();
-	}
-#endif
+
 	static GlobalErrorSignaler *instancePtr;
 	static LogItInstance *m_st_logIt;
 	static Log::LogComponentHandle m_st_lh;
-	boost::signals2::signal<void (const int,const char *,timeval &) > globalErrorSignal;
+	// boost::signals2::signal<void (const int,const char *,timeval &) > globalErrorSignal;
 
 public:
-	//boost::signals2::signal<void (const int,const char *,timeval &) > globalErrorSignal;
+	boost::signals2::signal<void (const int,const char *,timeval &) > globalErrorSignal;
 	SHARED_LIB_EXPORT_DEFN static GlobalErrorSignaler* getInstance();
 
+#if 0
 	// connect a given handler to the signal of the singleton. function pointer is the argument. Can have as amny connected handler as you want, but
 	// they will all be disconnected when the object goes out of scope (dtor)
 	void connectHandler( int (*fcnPtr)( int, const char* ) );
+#endif
 	// fire the signal with payload
 	void fireSignal( const int code, const char *msg );
 
