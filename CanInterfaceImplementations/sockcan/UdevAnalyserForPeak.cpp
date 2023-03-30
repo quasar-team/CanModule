a8ddb38610f9606d05a49e7ce0fe0ba01cf0113e
Michael Ludwig <Michael.Ludwig@cern.ch>
2023-03-30 14:21:24 +0200

    udev debugging for cal9

--- CanInterfaceImplementations/sockcan/UdevAnalyserForPeak.cpp
+++ CanInterfaceImplementations/sockcan/UdevAnalyserForPeak.cpp
@@ -269,8 +269,9 @@ unsigned int UdevAnalyserForPeak::m_peakLocalCanPort( std::string s ){
 	size_t pos2 = sub1.find( "/" ) + 1;
 	std::string sub2 = sub1.substr( pos2, sub1.find(" ") - 2 );
 	std::string sub3 = sub2.substr( 3, std::string::npos );
-	//std::cout << __FILE__ << " " << __LINE__ << " sub3= " << sub3 << std::endl;
+	std::cout << __FILE__ << " " << __LINE__ << " sub3= " << sub3 << std::endl;
 	int ii = std::stoi( sub3, 0, 10 );
+	std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << " ii= " << ii << std::endl;
 	return( (unsigned int) ii );
 }
 
@@ -297,7 +298,9 @@ unsigned int UdevAnalyserForPeak::m_peakSystemDeviceIndex( std::string s ){
 	size_t pos1 = s.find( "/" ) + 1;
 	std::string sub1 = s.substr( pos1, std::string::npos );
 	std::string sub2 = sub1.substr( 0, sub1.find("/") );
+	std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << " sub2= " << sub2 << std::endl;
 	int ii = std::stoi( sub2, 0, 10 );
+	std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << " ii= " << ii << std::endl;
 	return( (unsigned int) ii );
 }
 
