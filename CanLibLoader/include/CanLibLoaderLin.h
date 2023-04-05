141885662dcfd0c51b161f35cbf362573a3f737b
Michael Ludwig <Michael.Ludwig@cern.ch>
2023-04-05 16:19:59 +0200

    add global err signals windows and linux to libloader

--- CanLibLoader/include/CanLibLoaderLin.h
+++ CanLibLoader/include/CanLibLoaderLin.h
@@ -25,22 +25,24 @@
 
 namespace CanModule 
 {
-    class CanLibLoaderLin : public CanLibLoader
-    {
-     public:
+class CanLibLoaderLin : public CanLibLoader
+{
+public:
 	//Empty constructor
 	CanLibLoaderLin(const std::string& libName);
 
 	//Will cleanup the loaded dynamic library
 	virtual ~CanLibLoaderLin();
-     protected:
+protected:
 	//Load a dynamic library.
 	virtual void dynamicallyLoadLib(const std::string& libName);
 	//Uses the loaded library to create a HAL object and store it in p_halInstance
 	virtual CCanAccess*  createCanAccess();
-     private:
+
+private:
 	//Pointer to the dynamic library stored on the memory
 	void *p_dynamicLibrary;
+	GlobalErrorSignaler *m_gsig;
 
-    };
+};
 }
