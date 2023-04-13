d24f347df3e6a253e47ce95d3b5acf2e70390fa8
Michael Ludwig <Michael.Ludwig@cern.ch>
2023-04-13 14:14:05 +0200

    restructure Dll LogIt init for vendor libs, pass static ptr

--- CanLibLoader/src/CanLibLoader.cpp
+++ CanLibLoader/src/CanLibLoader.cpp
@@ -160,14 +160,15 @@ CCanAccess* CanLibLoader::openCanBus(std::string name, std::string parameters) {
 
 	// set the level when registering, component "CanModule"
 	// we check if the component is registered and the loglevel is set
-	Log::LOG_LEVEL loglevel = Log::TRC;
+	Log::LOG_LEVEL loglevel = Log::TRC; // default
 	Log::LogComponentHandle handle = Log::getComponentHandle( CanModule::LogItComponentName );
 	bool ret = Log::getComponentLogLevel( handle, loglevel );
 	if ( ret ) {
 		LOG(Log::DBG, lh ) << " got " << CanModule::LogItComponentName << " loglevel= " << loglevel;
 	} else {
 		LOG(Log::WRN, lh ) << " component " << CanModule::LogItComponentName << " does not exist, register it and set loglevel= " << loglevel;
-		logInstance->registerLoggingComponent( CanModule::LogItComponentName, loglevel );
+		// logInstance->registerLoggingComponent( CanModule::LogItComponentName, loglevel );
+		CanLibLoader::st_remoteLogIt->registerLoggingComponent( CanModule::LogItComponentName, loglevel );
 	}
 
 	LOG(Log::DBG, lh ) << __FUNCTION__ << " calling createBus. name= " << name << " parameters= " << parameters;
