90a1cda86f625cdf1943a930ad664c7ab57c251a
Michael Ludwig <michael.ludwig@cern.ch>
2023-05-12 16:52:04 +0200

    anagate2 diags

--- Diagnostic/include/Diag.h
+++ Diagnostic/include/Diag.h
@@ -37,9 +37,50 @@ public:
 		std::string parameter;
 	} CONNECTION_DIAG_t;
 
+
+	/** beta.v6 "an2" CANGetCounters anagate
+	The AnaGate CAN F series models can provide all nine TCP, CAN and error counters
+	from the web interface. A detailed description of the counters is available in the
+	device status chapter of the AnaGate CAN F series manual.
+	*/
+	typedef struct {
+		unsigned int countTCPRx; // TCP Received counter.
+		unsigned int countTCPTx; // TCP Transmitted counter.
+		unsigned int countCANRx; // CAN Received counter.
+		unsigned int countCANTx; // CAN Transmitted counter.
+		unsigned int countCANRxErr; // CAN Bus Receive Error counter.
+		unsigned int countCANTxErr; // CAN Bus Transmit Error counter.
+		unsigned int countCANRxDisc; // CAN Discarded Rx Full Queue counter.
+		unsigned int countCANTxDisc; // CAN Discarded Tx Full Queue counter.
+		unsigned int countCANTimeout; // CAN Transmit Timeout counter.
+	} PORT_COUNTERS_t;
+
+	typedef struct {
+		std::string message;
+		long int timestamp;
+	} PORT_LOG_ITEM_t;
+
+	typedef struct {
+		std::vector<PORT_LOG_ITEM_t> portLog;
+	} PORT_LOG_ALL_t;
+
+	typedef struct {
+		int temperature; // in deg C
+		int uptime;      // in seconds
+		unsigned int clientCount; // connected clients for this IP/module/submodule
+		std::vector<std::string> clientIps; // decoded into strings, from unsigned int
+		std::vector<unsigned int> clientPorts; // array of client ports
+		std::vector<long int> clientConnectionTimestamps; // (Unix time) of initial client connection
+	} HARDWARE_DIAG_t;
+
+
 	static void insert_maps( CanLibLoader *lib, CCanAccess *acc, std::string params );
 	static void delete_maps( CanLibLoader *lib, CCanAccess *acc );
 	static std::vector<Diag::CONNECTION_DIAG_t> get_connections();
+
+	static PORT_LOG_ITEM_t get_lastPortLogItem( CCanAccess *acc );
+	static PORT_LOG_ALL_t get_allPortLogItems( CCanAccess *acc );
+	static HARDWARE_DIAG_t get_hardwareDiag( CCanAccess *acc );
 
 	static int CanLibLoader_icount;
 	static int CanAccess_icount;
