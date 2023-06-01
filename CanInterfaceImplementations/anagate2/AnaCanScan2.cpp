e72a5c9687fd173b1bc4d0acbbd3d5670fac6c84
Michael Ludwig <Michael.Ludwig@cern.ch>
2023-06-01 15:58:54 +0200

    debug

--- CanInterfaceImplementations/anagate2/AnaCanScan2.cpp
+++ CanInterfaceImplementations/anagate2/AnaCanScan2.cpp
@@ -1198,19 +1198,15 @@ std::vector<CanModule::PORT_LOG_ITEM_t> AnaCanScan2::getHwLogMessages ( unsigned
  */
 CanModule::HARDWARE_DIAG_t AnaCanScan2::getHwDiagnostics (){
 	std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << std::endl;
-	int temperature;
-	int uptime;
-	int clientCount;
+	HARDWARE_DIAG_t d;
 	const unsigned int sz = 6;
 	unsigned int ips[ sz ];
 	unsigned int ports[ sz ];
 	AnaInt64 dates[ sz ];
 
-	std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << " m_UcanHandle= " << m_UcanHandle << std::endl;
-
-	AnaInt32 ret = CANGetDiagData( m_UcanHandle, &temperature, &uptime );
+	AnaInt32 ret = CANGetDiagData( m_UcanHandle, &d.temperature, &d.uptime );
 	std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << " ret= 0x" << hex << ret << std::endl;
-	AnaInt32 ret1 = CANGetClientList( m_UcanHandle, &clientCount, ips, ports, dates );
+	AnaInt32 ret1 = CANGetClientList( m_UcanHandle, &d.clientCount, ips, ports, dates );
 	std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << " ret1= 0x" << hex << ret1 << std::endl;
 	if ( ret != ANA_ERR_NONE || ret1 != ANA_ERR_NONE ){
 		MLOGANA2(ERR,this) << "There was a problem getting the HW DiagData and/or client list " << ret << " . Abandoning HW DiagData and client list retrieval ";
