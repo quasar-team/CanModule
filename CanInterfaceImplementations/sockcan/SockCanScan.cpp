940b600dd8cbdd770a66231b148823d2ad367bd6
mludwig <michael.ludwig@cern.ch>
2024-03-05 17:03:52 +0100

    minor cleanup

--- CanInterfaceImplementations/sockcan/SockCanScan.cpp
+++ CanInterfaceImplementations/sockcan/SockCanScan.cpp
@@ -21,7 +21,7 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with Quasar.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <SockCanScan.h>
+#include "SockCanScan.h"
 #include <UdevAnalyserForPeak.h>
 
 #include <thread>
