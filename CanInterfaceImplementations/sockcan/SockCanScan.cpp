5422e69df5c9e0ead0841ddf2df00744cd291051
mludwig <michael.ludwig@cern.ch>
2024-03-06 16:15:13 +0100

    suppress reconnectActionString TRC

--- CanInterfaceImplementations/sockcan/SockCanScan.cpp
+++ CanInterfaceImplementations/sockcan/SockCanScan.cpp
@@ -691,7 +691,7 @@ int CSockCanScan::m_openCanPort()
 			remaining_sleep_us = m_sendThrottleDelay;
 		}
 		if ( remaining_sleep_us > 0 ){
-			us_sleep( remaining_sleep_us );
+			CCanAccess::us_sleep( remaining_sleep_us );
 		}
 		m_previous = boost::posix_time::microsec_clock::local_time();
 	}
