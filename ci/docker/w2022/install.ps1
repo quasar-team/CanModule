0e472cf8ac03852dc0aed0a0d84f8ce52d6156c1
Luis Miguens Fernandez <luis.miguens@cern.ch>
2024-08-12 14:26:30 +0200

    Remove unused stuff

--- ci/docker/w2022/install.ps1
+++ ci/docker/w2022/install.ps1
@@ -16,10 +16,6 @@ choco install -y --no-progress visualstudio2022buildtools
 # C++ Component for Visual Studio 2022 with all extras
 choco install -y --no-progress visualstudio2022-workload-vctools
 
-# Link Professional folder to BuildTools for compatibility with existing
-# building scripts
-New-Item -ItemType SymbolicLink -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional" -Target "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools"
-
 # Clean-up
 Remove-Item -Path "C:\Program Files (x86)\Microsoft Visual Studio\Installer" -Recurse -Force
 Remove-Item -Path "C:\Users\ContainerAdministrator\AppData\Local\Temp\chocolatey" -Recurse -Force
