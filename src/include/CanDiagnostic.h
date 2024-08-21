23f04968458543df3a07c00e5cf492ba19208246
Luis Miguens Fernandez <luis.miguens@cern.ch>
2024-08-21 10:48:53 +0200

    Initial struct for CanDiagnostic

--- src/include/CanDiagnostic.h
+++ src/include/CanDiagnostic.h
@@ -1,17 +1,74 @@
 #ifndef SRC_INCLUDE_CANDIAGNOSTIC_H_
 #define SRC_INCLUDE_CANDIAGNOSTIC_H_
 
+#include <optional>
 #include <string>
 #include <vector>
 
-#include "CanVersion.h"
+using optional_string_vector_t = std::optional<std::vector<std::string>>;
+using optional_string_t = std::optional<std::string>;
+using optional_uint32_vector_t = std::optional<std::vector<u_int32_t>>;
 
+/**
+ * @brief CanDiagnostic struct to store diagnostic information.
+ *
+ * This struct contains various fields to store diagnostic information related
+ * to CAN communication. The fields are wrapped in std::optional to indicate
+ * that they may not always be available.
+ */
 struct CanDiagnostic {
-  std::vector<std::string> log_entries;
-  std::string last_error;
-  std::vector<std::string> connected_clients;
-  constexpr std::string_view canmodule_version{CanModule::Version::value};
-  constexpr std::string_view canmodule_date{CanModule::Version::date};
+  optional_string_vector_t log_entries;  ///< Optional vector of log entries.
+
+  optional_string_t name;     ///< Optional SocketCAN device name.
+  std::optional<int> handle;  ///< Optional handle for Anagate devices.
+  optional_string_t
+      mode;  ///< Optional mode for both SocketCAN and Anagate devices.
+  optional_string_t
+      state;  ///< Optional state for both SocketCAN and Anagate devices.
+  std::optional<u_int32_t>
+      bitrate;  ///< Optional bitrate for both SocketCAN and Anagate devices.
+
+  optional_string_vector_t
+      connected_clients_addresses;  ///< Optional vector of connected clients'
+                                    ///< addresses for Anagate devices.
+  optional_string_vector_t
+      connected_clients_timestamps;  ///< Optional vector of connected clients'
+                                     ///< timestamps for Anagate devices.
+  optional_uint32_vector_t
+      connected_clients_ports;  ///< Optional vector of connected clients' ports
+                                ///< for Anagate devices.
+  std::optional<u_int32_t>
+      number_connected_clients;  ///< Optional number of connected clients for
+                                 ///< Anagate devices.
+
+  std::optional<float>
+      temperature;  ///< Optional temperature reading for Anagate devices.
+  std::optional<u_int32_t> uptime;  ///< Optional uptime for Anagate devices.
+
+  std::optional<u_int32_t> countTCPRx;  ///< Optional TCP Received counter for
+                                        ///< both SocketCAN and Anagate devices.
+  std::optional<u_int32_t>
+      countTCPTx;  ///< Optional TCP Transmitted counter for both SocketCAN and
+                   ///< Anagate devices.
+  std::optional<u_int32_t> countCANRx;  ///< Optional CAN Received counter for
+                                        ///< both SocketCAN and Anagate devices.
+  std::optional<u_int32_t>
+      countCANTx;  ///< Optional CAN Transmitted counter for both SocketCAN and
+                   ///< Anagate devices.
+  std::optional<u_int32_t>
+      countCANRxErr;  ///< Optional CAN Bus Receive Error counter for both
+                      ///< SocketCAN and Anagate devices.
+  std::optional<u_int32_t>
+      countCANTxErr;  ///< Optional CAN Bus Transmit Error counter for both
+                      ///< SocketCAN and Anagate devices.
+  std::optional<u_int32_t>
+      countCANRxDisc;  ///< Optional CAN Discarded Rx Full Queue counter for
+                       ///< Anagate devices.
+  std::optional<u_int32_t>
+      countCANTxDisc;  ///< Optional CAN Discarded Tx Full Queue counter for
+                       ///< Anagate devices.
+  std::optional<u_int32_t> countCANTimeout;  ///< Optional CAN Transmit Timeout
+                                             ///< counter for Anagate devices.
 };
 
 #endif  // SRC_INCLUDE_CANDIAGNOSTIC_H_
