2b3d5474d734b3f59598521d86bb96821267f442
Luis Miguens Fernandez <luis.miguens@cern.ch>
2024-09-02 15:56:52 +0200

    Document CanLogIt

--- src/include/CanLogIt.h
+++ src/include/CanLogIt.h
@@ -4,7 +4,24 @@
 #include "CanVersion.h"
 #include "LogIt.h"
 
+/**
+ * @brief A utility class for logging within the CAN Module.
+ *
+ * This class provides a static function to initialize and register a logging
+ * component for the CAN Module. The function ensures that the logging
+ * initialization and registration only happen once using a static local
+ * variable. It initializes the logging system using Log::initializeLogging()
+ * and then registers a logging component named "CanModule" using
+ * Log::registerLoggingComponent(). Afterwards, it logs the SHA and date of the
+ * CAN Module using the registered logging component.
+ */
 struct CanLogIt {
+  /**
+   * @brief This function initializes and registers a logging component for the
+   * CAN Module.
+   *
+   * @return A handle to the registered logging component.
+   */
   inline static Log::LogComponentHandle h() {
     static Log::LogComponentHandle handle = [] {
       Log::initializeLogging();
