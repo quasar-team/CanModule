866b7f2159eede63532483bc0ab54ee4b4fd1361
Luis Miguens Fernandez <luis.miguens@cern.ch>
2024-11-13 15:47:12 +0100

    WIP

--- src/main/CanDerivedStats.cpp
+++ src/main/CanDerivedStats.cpp
@@ -1,5 +1,7 @@
 #include "CanDerivedStats.h"
 
+#include <deque>
+
 #include "CanLogIt.h"
 
 void CanDerivedStats::update(CanDiagnostics &diagnostics) noexcept {
@@ -35,37 +37,31 @@ void CanDerivedStats::update(CanDiagnostics &diagnostics) noexcept {
                        .count();
 
   if (time_diff > 0) {
-    double rx_rate =
-        static_cast<double>(diagnostics.rx.value() - m_rx) / time_diff;
+    const auto update_rate = [&](std::deque<double> &window,
+                                 std::optional<uint32_t> &value,
+                                 uint32_t &last_value, int64_t time_diff) {
+      double rate = static_cast<double>(value.value() - last_value) / time_diff;
+      window.push_front(rate);
+      if (window.size() > window_size) {
+        window.pop_back();
+      }
+      last_value = value.value();
+    };
 
-    double tx_rate =
-        static_cast<double>(diagnostics.tx.value() - m_tx) / time_diff;
+    update_rate(m_rx_per_second_window, diagnostics.rx, m_rx, time_diff);
+    update_rate(m_tx_per_second_window, diagnostics.tx, m_tx, time_diff);
 
-    m_rx_per_second_window.push_front(rx_rate);
-    m_tx_per_second_window.push_front(tx_rate);
-
-    if (m_rx_per_second_window.size() > window_size) {
-      m_rx_per_second_window.pop_back();
-    }
-    if (m_tx_per_second_window.size() > window_size) {
-      m_tx_per_second_window.pop_back();
-    }
-
-    m_rx = diagnostics.rx.value();
-    m_tx = diagnostics.tx.value();
     m_last_update = current_time;
 
-    if (m_rx_per_second_window.size() > 0) {
-      diagnostics.rx_per_second =
-          std::accumulate(m_rx_per_second_window.begin(),
-                          m_rx_per_second_window.end(), 0.0) /
-          m_rx_per_second_window.size();
-    }
-    if (m_tx_per_second_window.size() > 0) {
-      diagnostics.tx_per_second =
-          std::accumulate(m_tx_per_second_window.begin(),
-                          m_tx_per_second_window.end(), 0.0) /
-          m_tx_per_second_window.size();
-    }
+    const auto update_per_second = [&](const std::deque<double> &window,
+                                       std::optional<double> &per_second) {
+      if (!window.empty()) {
+        per_second =
+            std::accumulate(window.begin(), window.end(), 0.0) / window.size();
+      }
+    };
+
+    update_per_second(m_rx_per_second_window, diagnostics.rx_per_second);
+    update_per_second(m_tx_per_second_window, diagnostics.tx_per_second);
   }
 }
