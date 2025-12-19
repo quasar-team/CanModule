0d9b446fb568d0726b9753f8a5fd1ba6cf7a5181
Luis Miguens Fernandez <luis.miguens@cern.ch>
2025-12-19 11:37:09 +0100

    Fix test

--- test/python/test_anagate.py
+++ test/python/test_anagate.py
@@ -181,7 +181,7 @@ def test_anagate_diagnostics():
     stats1 = myDevice1.diagnostics()
     stats2 = myDevice2.diagnostics()
 
-    assert len(stats1.connected_clients_ports) > 1
+    assert len(stats1.connected_clients_ports) >= 1
 
     assert stats1.tx_per_second is None
     assert stats2.rx_per_second is None
