42a0cde631c2df1a99aefb8ac48e7240abe08fed
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-01-28 12:22:40 +0100

    .

--- cmake/logit.cmake
+++ cmake/logit.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
   LogIt
   GIT_REPOSITORY https://github.com/quasar-team/LogIt.git
-  GIT_TAG        master
+  GIT_TAG        migration-new-pipeline
   DOWNLOAD_EXTRACT_TIMESTAMP True
 )
 FetchContent_MakeAvailable(LogIt)
