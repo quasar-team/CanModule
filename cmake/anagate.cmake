e0f119db223c1afd8914a80e03d6d5664b7fb777
Luis Miguens Fernandez <luis.miguens@cern.ch>
2026-02-24 19:59:49 +0100

    OPCUA-3303 Provide a system to indicate a zip file or another URL for Anagate API (#101)

--- cmake/anagate.cmake
+++ cmake/anagate.cmake
@@ -1,18 +1,32 @@
 include(FetchContent)
 
-set(ANAGATE_LIBRARY "https://ics-deps-repo.web.cern.ch/canmodule/anagate/20241023/AnaGateAPI-CAN-2.5-BETA-3.zip")
-
-if(NOT DEFINED ENV{ICS_REPO_DEPS_TOKEN})
-    message(FATAL_ERROR "Environment variable ICS_REPO_DEPS_TOKEN is not set. Please set this variable to proceed.")
+if(NOT DEFINED ANAGATE_LIBRARY)
+    set(ANAGATE_LIBRARY "https://ics-deps-repo.web.cern.ch/canmodule/anagate/20241023/AnaGateAPI-CAN-2.5-BETA-3.zip")
 endif()
 
-message(STATUS "Download URL: ${ANAGATE_LIBRARY}")
+set(ANAGATE_FETCHCONTENT_ARGS
+    URL "${ANAGATE_LIBRARY}"
+    DOWNLOAD_EXTRACT_TIMESTAMP True
+)
+
+if(EXISTS "${ANAGATE_LIBRARY}")
+    message(STATUS "Using local Anagate archive: ${ANAGATE_LIBRARY}")
+elseif(ANAGATE_LIBRARY MATCHES "^https?://")
+    if(DEFINED ENV{ICS_REPO_DEPS_TOKEN} AND NOT "$ENV{ICS_REPO_DEPS_TOKEN}" STREQUAL "")
+        message(STATUS "Downloading Anagate archive with authentication: ${ANAGATE_LIBRARY}")
+        list(APPEND ANAGATE_FETCHCONTENT_ARGS
+            HTTP_HEADER "PRIVATE-TOKEN: $ENV{ICS_REPO_DEPS_TOKEN}"
+        )
+    else()
+        message(STATUS "Downloading Anagate archive without authentication: ${ANAGATE_LIBRARY}")
+    endif()
+else()
+    message(FATAL_ERROR "ANAGATE_LIBRARY must be an existing local archive or an http(s) URL. Got: ${ANAGATE_LIBRARY}")
+endif()
 
 FetchContent_Declare(
     Anagate
-    URL ${ANAGATE_LIBRARY}
-    HTTP_HEADER "PRIVATE-TOKEN: $ENV{ICS_REPO_DEPS_TOKEN}"
-    DOWNLOAD_EXTRACT_TIMESTAMP True
+    ${ANAGATE_FETCHCONTENT_ARGS}
 )
 
 FetchContent_MakeAvailable(Anagate)
